218eba3234bf920b9724e649fb09c861c96be899
BoGoBooM <myPikcha@gmail.com>
2025-05-27 13:52:55 +0300

    make build

---
+++ index.ffd75416.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector(".contact-us__form").addEventListener("submit",function(e){e.preventDefault(),document.querySelector(".contact-us__form").reset()});
+//# sourceMappingURL=index.ffd75416.js.map
